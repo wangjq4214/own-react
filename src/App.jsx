c6f97b35909f25b583e74491bab77caab14c8498
wangjq4214 <w1637894214@163.com>
2021-01-27 23:01:22 +0800

    perf: app 中增加 props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Own from './own';
 
 /** @jsx Own.createElement */
-const Element = () => (
+const Element = props => (
   <div style="background: salmon">
-    <h1>Hello World</h1>
+    <h1>Hello {props.name}</h1>
     <h2 style="text-align:right">from WJQ</h2>
   </div>
 );
 
-export const App = <Element />;
+export const App = <Element name="World" />;
